789465593c4e5b9f5a5a99314dd28c94ecf8d829
Mikołaj Pańka <01149377@pw.edu.pl>
2021-09-18 17:17:19 +0200

    Start Main and ViterbiBMP.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Path imagePath = Paths.get("assets", "bee100.bmp");
+        if (args.length > 1) imagePath = Paths.get(args[1]);
+
+        Path textPath = Paths.get("assets", "bee100.txt");
+        if (args.length > 2) textPath = Paths.get(args[2]);
+
+        Path outImgPath = Paths.get("assets", "out100.bmp");
+        if (args.length > 3) outImgPath = Paths.get(args[3]);
+
+        int errorRate = 10;
+        if (args.length > 4) errorRate = Integer.parseInt(args[4]);
+
+        Path outIS95Path = Paths.get("assets", "outIS95.txt");
+        if (args.length > 5) outImgPath = Paths.get(args[5]);
+
+        int decodingDepth = 24;
+        if (args.length > 6) decodingDepth = Integer.parseInt(args[6]);
+
+        Path decodedImgPath = Paths.get("assets", "outIS95.bmp");
+        if (args.length > 7) decodedImgPath = Paths.get(args[7]);
     }
 }
